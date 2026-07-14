2ce4e9483135bcb9ca87b0a22aeac0777ba05ef0
Viswazgummadi <viswazgummadi@gmail.com>
2026-07-14 17:23:26 +0530

    done

---
+++ intermediates/vector_add.cudafe1.c
@@ -0,0 +1,6 @@
+# 1 "src/vector_add.cu"
+# 61 "/usr/include/c++/11/iostream" 3
+extern _ZSt7ostream _ZSt4cout __attribute__((visibility("default")));
+# 74 "/usr/include/c++/11/iostream" 3
+static struct _ZNSt8ios_base4InitE _ZN44_INTERNAL_efaba54b_13_vector_add_cu_3af7cba3St8__ioinitE __attribute__((visibility("default"))) = {};
+extern void *__dso_handle __attribute__((visibility("hidden")));
